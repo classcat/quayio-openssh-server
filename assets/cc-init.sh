ba7f44457bf8d711ab824f840777b0e5c29165bc
ClassCat <support@classcat.com>
2015-06-02 00:07:38 +0900

    cc-init.sh added

---
+++ assets/cc-init.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+########################################################################
+# ClassCat/OpenSSH-Server Asset files
+# Copyright (C) 2015 ClassCat Co.,Ltd. All rights reserved.
+########################################################################
+
+#--- HISTORY -----------------------------------------------------------
+# 02-jun-15 : fixed.
+#-----------------------------------------------------------------------
+
+
+######################
+### INITIALIZATION ###
+######################
+
+function init () {
+  echo "ClassCat Info >> initialization code for ClassCat/OpenSSH-Server"
+  echo "Copyright (C) 2015 ClassCat Co.,Ltd. All rights reserved."
+  echo ""
+}
+
+
+############
+### SSHD ###
+############
+
+function change_root_password() {
+  if [ -z "${ROOT_PASSWORD}" ]; then
+    echo "ClassCat Warning >> No ROOT_PASSWORD specified."
+  else
+    echo -e "root:${ROOT_PASSWORD}" | chpasswd
+    # echo -e "${password}\n${password}" | passwd root
+  fi
+}
+
+
+function put_public_key() {
+  if [ -z "$SSH_PUBLIC_KEY" ]; then
+    echo "ClassCat Warning >> No SSH_PUBLIC_KEY specified."
+  else
+    mkdir -p /root/.ssh
+    chmod 0700 /root/.ssh
+    echo "${SSH_PUBLIC_KEY}" > /root/.ssh/authorized_keys
+  fi
+}
+
+
+##################
+### SUPERVISOR ###
+##################
+# See http://docs.docker.com/articles/using_supervisord/
+
+function proc_supervisor () {
+  cat > /etc/supervisor/conf.d/supervisord.conf <<EOF
+[program:ssh]
+command=/usr/sbin/sshd -D
+
+[program:rsyslog]
+command=/usr/sbin/rsyslogd -n
+EOF
+}
+
+
+### ENTRY POINT ###
+
+init
+change_root_password
+put_public_key
+proc_supervisor
+
+# /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+
+exit 0
+
+
+### End of Script ###
